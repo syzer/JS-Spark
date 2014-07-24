7dadfad10e66c584831eff33527afb640709117c
syzer <syzer3@gmail.com>
2014-07-24 22:37:18 +0200

    package name started parser for CSV files

---
+++ private/src/server/service/file/parser.js
@@ -0,0 +1,13 @@
+/**
+ * Created by syzer on 7/24/2014.
+ */
+module.exports = function jsSParkService() {
+
+    return  {
+
+        parse: function(filePath) {
+            //return generator
+        }
+
+    }
+};
