5cb5ced4dd0b455fc86996e3fb7a9159f04b7c5e
syzer <syzer3@gmail.com>
2014-10-03 11:19:38 +0200

    sligth refactoring

---
+++ private/src/server/service/taskManager.js
@@ -0,0 +1,22 @@
+module.exports = function taskManager(dispatcher) {
+
+    // private
+    // TODO task is duplicated in dispacher
+    var tasks = [];
+
+    return {
+        init: init,
+        addTask: addTask
+    };
+
+    // public methods
+    function init() {
+      dispatcher.start()
+    }
+
+    function addTask(task) {
+      dispatcher.addTask(task);
+      tasks.push(task)
+    }
+
+};
